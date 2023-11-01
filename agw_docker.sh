d6d352f5c8a2bc3ebd5ab0f04b3d761245679d38
sriramgopalvarma <121871187+sriramgopalvarma@users.noreply.github.com>
2023-11-01 16:22:01 +0530

    Create agw_docker.sh

---
+++ agw_docker.sh
@@ -0,0 +1,20 @@
+# changing intefaces name
+  sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub
+  sed -i 's/ens5/eth0/g; s/ens6/eth1/g' /etc/netplan/50-cloud-init.yaml
+  # changing interface name
+  update-grub2
+
+  if ! grep -q "eth1" /etc/netplan/50-cloud-init.yaml; then
+    cat > /etc/netplan/70-secondary-itf.yaml << EOF
+    network:
+        ethernets:
+            eth1:
+                dhcp4: true
+                dhcp6: false
+                dhcp4-overrides:
+                  route-metric: 200
+        version: 2
+EOF
+  fi
+  netplan apply
+fi
