34c14f4f3e20ab44fa632df8105f0bde4038bdff
sriramgopalvarma <121871187+sriramgopalvarma@users.noreply.github.com>
2023-11-01 16:33:06 +0530

    Update agw_docker.sh

--- agw_docker.sh
+++ agw_docker.sh
@@ -1,8 +1,5 @@
 # changing intefaces name
-  sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub
   sed -i 's/enp1s0/eth0/g; s/enp2s0/eth1/g' /etc/netplan/50-cloud-init.yaml
-  # changing interface name
-  update-grub2
 
   if ! grep -q "eth1" /etc/netplan/50-cloud-init.yaml; then
     cat > /etc/netplan/70-secondary-itf.yaml << EOF
