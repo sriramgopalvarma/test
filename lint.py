349687157ff8fe336c4c5f2d2b2e46f3bb165afc
sriramgopalvarma <121871187+sriramgopalvarma@users.noreply.github.com>
2024-02-22 11:38:53 +0530

    Create lint.py

---
+++ lint.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+import sys
+
+def lint_directory(directory):
+    for dirpath, _, filenames in os.walk(directory):
+        for filename in filenames:
+            if filename.endswith('.py'):
+                file_path = os.path.join(dirpath, filename)
+                subprocess.run(['pylint', file_path])
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python lint.py directory_path")
+        sys.exit(1)
+    
+    directory_to_lint = sys.argv[1]
+    lint_directory(directory_to_lint)
